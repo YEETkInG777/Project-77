320102443e20e20e8d56b4a73c5e8be258b51b20
YEETkInG777 <76080617+YEETkInG777@users.noreply.github.com>
2021-01-28 19:57:26 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,22 @@
+<script>
+console.log("my first console project");
+</script>
+var first_name = "Dhairya";
+var last_name = "Thakor";
+
+var full_name = first_name.contact(last_name); console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add)
+
+var sub = a - b;
+console.log(sub)
+
+var multiple = a * b;
+console.log(multiple);
+
+var division = a / b;
+console.log(division);
